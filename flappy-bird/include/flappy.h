8b7efff1fab1d0b5c887b55e06e2f0b2d2736809
martinhundrup <hundrupm@gmail.com>
2025-07-30 07:17:59 -0700

    fixed tasks directories

--- flappy-bird/include/flappy.h
+++ flappy-bird/include/flappy.h
@@ -9,10 +9,10 @@
 #ifndef FLAPPY_H
 #define FLAPPY_H
 
-#include "../ascii-engine/include/ascii-engine.h"
-#include "../ascii-engine/include/input.h"
-#include "../ascii-engine/include/gametick.h"
-#include "../ascii-engine/include/draw.h"
+#include "../../ascii-engine/include/ascii-engine.h"
+#include "../../ascii-engine/include/input.h"
+#include "../../ascii-engine/include/gametick.h"
+#include "../../ascii-engine/include/draw.h"
 
 typedef struct {
   Vector2_Int position;
